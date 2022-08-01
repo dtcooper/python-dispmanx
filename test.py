193df70a3cf5d0a4f40dcd6421e314eb1da62849
David Cooper <david@dtcooper.com>
2022-08-01 05:19:28 -0400

    Update demo

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import os
-os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+import random
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"  # noqa
 
 import pygame
 
 from dispmanx import Dispmanx
+
+
+
+def random_color():
+    return tuple(random.randint(0, 0xFF) for _ in range(3))
 
 
 print("Found displays:")
@@ -21,23 +28,27 @@ print("Running demo...")
 width, height = dispmanx.size
 surface = pygame.image.frombuffer(dispmanx.buffer, dispmanx.size, dispmanx.format)
 
-bg_color = (0xFF, 0xEE, 0x00)
-circle_color = (0x5F, 0xE8, 0xFF)
-x_color = (0xFC, 0x49, 0xAB)
-square_length = 50
+bg_color = random_color()
+circle_color = random_color()
+x_color = random_color()
+
+square_length = 60
 circle_radius = 100
-line_width = 10
+line_width = 25
 
 clock = pygame.time.Clock()
 
-for opacity in reversed(range(0, 256)):
-    surface.fill(bg_color + (opacity,))
+for fade_out in (False, True) * 10:
+    for opacity in reversed(range(0, 256)) if fade_out else range(0, 256):
+        surface.fill(bg_color + (opacity,))
 
-    pygame.draw.circle(surface, circle_color, (width / 2, height / 2), circle_radius, line_width)
-    pygame.draw.line(surface, x_color, (0, height - 1), (width - 1, 0), line_width)
-    pygame.draw.line(surface, x_color, (0, 0), (width - 1, height - 1), line_width)
-
-    dispmanx.update()
-    clock.tick(24)
+        pygame.draw.line(surface, x_color, (0, height - 1), (width - 1, 0), line_width)
+        pygame.draw.line(surface, x_color, (0, 0), (width - 1, height - 1), line_width)
+        pygame.draw.circle(surface, circle_color, (width / 2, height / 2), circle_radius, line_width)
+        dispmanx.update()
+        clock.tick(150)
+    if fade_out:
+        bg_color = random_color()
+    print(f"FPS: {clock.get_fps()}")
 
 print("Demo complete. Exiting")
