0494bcf5967c4b31ccaa748b8ca38b5603f651c7
David Cooper <david@dtcooper.com>
2022-08-06 18:43:08 -0400

    Test release

--- dispmanx/__init__.py
+++ dispmanx/__init__.py
@@ -1,7 +1,7 @@
 from .dispmanx import DispmanX, DispmanXError, DispmanXRuntimeError
 
 
-__version__ = "0.0.1-dev5"
+__version__ = "0.0.1-dev6"  # Make sure this is updated in pyproject.toml as well
 __all__ = [
     "DispmanX",
     "DispmanXError",
