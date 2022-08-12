22d956cb674af650b1d3346096151159f0bfb458
David Cooper <david@dtcooper.com>
2022-08-12 19:01:21 -0400

    Clean up numpy static example

--- examples/numpy_static.py
+++ examples/numpy_static.py
@@ -2,14 +2,14 @@ import time
 import numpy
 from dispmanx import DispmanX
 
-display = DispmanX(pixel_format="RGB565", buffer_type="numpy")
+import numpy
 
-buf = display.buffer
-high = numpy.iinfo(buf.dtype).max + 1
+display = DispmanX(pixel_format="RGB565", buffer_type="numpy")
+high = numpy.iinfo(display.buffer.dtype).max + 1  # white pixel
+rng = numpy.random.default_rng()
 
 while True:
-    # Generate random colors, simulating old TV static
-    static = numpy.random.randint(0, high, size=buf.size, dtype=buf.dtype).reshape(buf.shape)
-    numpy.copyto(buf, static)
+    static = rng.integers(low=0, high=high, size=display.buffer.shape, dtype=display.buffer.dtype)
+    numpy.copyto(display.buffer, static)  # Simulates TV static
     display.update()
     time.sleep(0.0135)
